03ea0a13a4ad140b316f51b70b087f83144d26e4
jeromea33 <raizumaraiz@gmail.com>
2014-01-11 13:11:19 +0800

    Initial APPS Commit

---
+++ src/APPS/MAIN.java
@@ -0,0 +1,16 @@
+
+package APPS;
+
+/**
+ * @author Team AMAZING!
+ */
+
+import java.awt.Font;
+
+public class MAIN {
+    public static String ConnectionString /*= "Insert STRING HERE"*/;
+    public static void main(String[] args) {
+        
+    }
+    
+}
